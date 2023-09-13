00f23f8014e2ba9b396dc6718dd7eb437e726a7c
hyeokson <cksgur1765@konkuk.ac.kr>
2023-09-13 18:03:04 +0900

    first commit

---
+++ src/main/java/com/konkuk/vecto/VectoApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.vecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/vecto/VectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.vecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
