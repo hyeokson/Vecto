dde5849ef768002cebf2457f39923e127a9c528c
jhbaik1501 <jhbaik1501@gmail.com>
2023-10-17 00:26:34 +0900

    fix: 모든 요청에 대해 Security를 적용하지 않도록 변경 (임시)

--- src/main/java/com/konkuk/vecto/security/config/WebSecurityConfig.java
+++ src/main/java/com/konkuk/vecto/security/config/WebSecurityConfig.java
@@ -39,7 +39,9 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // 정적 자원에 대해서 Security를 적용하지 않음으로 설정
-        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+            .and()
+            .ignoring().requestMatchers("/**");
     }
 
     /**
@@ -159,7 +161,6 @@ public class WebSecurityConfig {
      *
      * @return JwtAuthorizationFilter
      */
-    @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
     }
