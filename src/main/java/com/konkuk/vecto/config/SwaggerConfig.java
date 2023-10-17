d4d8bebf0574c72e5be36a7d0bb572d0199bc52c
jhbaik1501 <jhbaik1501@gmail.com>
2023-10-17 19:25:46 +0900

    fix: swagger 설정 변경

--- src/main/java/com/konkuk/vecto/config/SwaggerConfig.java
+++ src/main/java/com/konkuk/vecto/config/SwaggerConfig.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 @OpenAPIDefinition(
 	info = @Info(title = "Vecto App",
 		description = "Vecto app api명세",
-		version = "v1"))
+		version = "v1"),
+	servers = {
+		@Server(url = "https://vec-to.net"),
+		@Server(url = "http://localhost:5000")
+	}
+)
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
