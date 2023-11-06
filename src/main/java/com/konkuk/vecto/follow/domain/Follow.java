16adf0420bb4894a6828fba9e846f61b0c3b9ca5
hyeokson <cksgur1765@konkuk.ac.kr>
2023-11-07 00:41:20 +0900

    NoargsConstructor 추가

--- src/main/java/com/konkuk/vecto/follow/domain/Follow.java
+++ src/main/java/com/konkuk/vecto/follow/domain/Follow.java
@@ -3,11 +3,13 @@ package com.konkuk.vecto.follow.domain;
 import com.konkuk.vecto.security.domain.User;
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Table(name = "FOLLOW")
+@NoArgsConstructor
 public class Follow {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
