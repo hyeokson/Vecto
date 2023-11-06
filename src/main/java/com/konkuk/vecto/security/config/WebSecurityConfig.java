3758b812294894f46c66d54be2b716816239cb1c
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2023-11-06 14:22:32 +0900

    feat: 소개 페이지 제작

--- src/main/java/com/konkuk/vecto/security/config/WebSecurityConfig.java
+++ src/main/java/com/konkuk/vecto/security/config/WebSecurityConfig.java
@@ -44,7 +44,8 @@ public class WebSecurityConfig {
                 .requestMatchers("/swagger").requestMatchers("/v3/**")
                 .requestMatchers("/userId/check").requestMatchers("/login")
                 .requestMatchers("/mail")
-            .requestMatchers(HttpMethod.GET, "/feed/**");
+                .requestMatchers(HttpMethod.GET, "/feed/**")
+                .requestMatchers(HttpMethod.GET, "", "/", "/introduction.html");
     }
 
     /**
