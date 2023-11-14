72d5c8c6b8f33505f5a0e055c3942fb655f9a2b2
hyeokson <cksgur1765@konkuk.ac.kr>
2023-11-15 02:46:09 +0900

    FeedPatch 수정 2

---
+++ src/main/java/com/konkuk/vecto/feed/repository/FeedMapImageRepository.java
@@ -0,0 +1,13 @@
+package com.konkuk.vecto.feed.repository;
+
+import com.konkuk.vecto.feed.domain.Feed;
+import com.konkuk.vecto.feed.domain.FeedImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface FeedMapImageRepository extends JpaRepository<FeedImage, Long> {
+    void deleteByFeed(Feed feed);
+
+}
