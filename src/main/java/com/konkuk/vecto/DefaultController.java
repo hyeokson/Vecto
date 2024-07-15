74918528b672a05c9f9154adbaf9edf39c7a01eb
hyeokson <chanhuk1765@gmail.com>
2024-07-15 18:08:50 +0900

    refactor: 리팩토링 for hyundai project

--- src/main/java/com/konkuk/vecto/DefaultController.java
+++ src/main/java/com/konkuk/vecto/DefaultController.java
@@ -1,13 +1,28 @@
 package com.konkuk.vecto;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.net.http.HttpRequest;
+import java.util.Collections;
+
 @Controller
+@Slf4j
 public class DefaultController {
 
 	@GetMapping("/")
-	public String introduction() {
+	public String introduction(HttpServletRequest httpRequest) {
+		log.debug("Request URL: {}", httpRequest.getRequestURL().toString());
+
+		// 모든 헤더의 이름과 값을 로깅
+		Collections.list(httpRequest.getHeaderNames())
+				.forEach(headerName -> {
+					String headerValue = Collections.list(httpRequest.getHeaders(headerName)).toString();
+					log.debug("HeaderName: {}, HeaderValue: {}",headerName, headerValue);
+				});
+
 		return "introduction";
 	}
 
